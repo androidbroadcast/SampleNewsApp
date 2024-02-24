dae1bb902eaa6c3004f7e5d490f792000fe7d0ff
kirich1409 <kirill@androidbroadcast.dev>
2024-02-24 12:10:01 +0300

    Setup Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.jetbrainsKotlinJvm) apply false
+    alias(libs.plugins.kotlinSerialization) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.ksp) apply false
+}
