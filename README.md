84195d78dda26901d72607ed4f9c3b5bfd649d73
kirich1409 <kirill@androidbroadcast.dev>
2024-09-19 21:53:30 +0300

    Code for video sample

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
+### Module Graph
+
+```mermaid
+%%{
+  init: {
+    'theme': 'neutral'
+  }
+}%%
+
+graph LR
+  subgraph :core
+    :core:common["common"]
+    :core:uikit["uikit"]
+  end
+  subgraph :features:news-main
+    :features:news-main:ui["ui"]
+  end
+  :shared --> :compose-app
+  :compose-app --> :features:news-main:ui
+  :compose-app --> :core:common
+  :compose-app --> :core:uikit
+  :app --> :compose-app
+  :desktop --> :compose-app
+
+classDef focus fill:#769566,stroke:#fff,stroke-width:2px,color:#fff;
+class :compose-app focus
+```
 # Новостное приложение
 
 Новостное приложения со [стримов Android Broadcast](https://www.youtube.com/playlist?list=PL0SwNXKJbuNmIqMPiBnXkmfugSjWePAmx)
--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    id("dev.iurysouza.modulegraph") version "0.10.1"
 }
 
 // TODO Replace Detekt Gradle with terminal launch
@@ -52,3 +53,50 @@ allprojects.onEach { project ->
         }
     }
 }
+
+moduleGraphConfig {
+    readmePath.set("./README.md")
+    heading = "### Module Graph"
+    // showFullPath.set(false) // optional
+    // orientation.set(Orientation.LEFT_TO_RIGHT) //optional
+    // linkText.set(LinkText.NONE) // optional
+    // setStyleByModuleType.set(true) // optional
+    // excludedConfigurationsRegex.set(".*test.*") // optional
+    // excludedModulesRegex.set(".*moduleName.*") // optional
+    // focusedModulesRegex.set(".*(projectName).*") // optional
+    // rootModulesRegex.set(".*moduleName.*") // optional
+    // theme.set(Theme.NEUTRAL) // optional
+    // or you can fully customize it by using the BASE theme:
+    // Theme.BASE(
+    //     themeVariables = mapOf(
+    //         "primaryTextColor" to "#F6F8FAff", // Text
+    //         "primaryColor" to "#5a4f7c", // Node
+    //         "primaryBorderColor" to "#5a4f7c", // Node border
+    //         "tertiaryColor" to "#40375c", // Container box background
+    //         "lineColor" to "#f5a623",
+    //         "fontSize" to "12px",
+    //     ),
+    //     focusColor = "#F5A622",
+    //     moduleTypes = listOf(
+    //         ModuleType.AndroidLibrary("#2C4162"),
+    //     )
+    // ),
+    // )
+
+    // You can add additional graphs.
+    // A separate graph will be generated for each config below.
+    // graph(
+    //     readmePath = "./README.md",
+    //     heading = "# Graph with root: gama",
+    // ) {
+    //     rootModulesRegex = ".*gama.*"
+    // }
+    // graph(
+    //     readmePath = "./SomeOtherReadme.md",
+    //     heading = "# Graph",
+    // ) {
+    //     rootModulesRegex = ".*zeta.*"
+    // }
+    focusedModulesRegex.set(".*(compose-app).*")
+}
+
